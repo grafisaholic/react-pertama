e85f31fedc41caa8ef06049655ab3983dbbd07d1
grafisaholic <khabiburrokhman13@gmail.com>
2021-01-19 15:03:06 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import Table from './Table'
+import Form from './Form'
+class App extends Component {
+  state = {
+    jobs : [
+      {
+        job: 'Progammer',
+        salary: '50.000.000'
+      },
+      {
+        job: 'Bakul Bakso',
+        salary: '1.000.000'
+      },
+      {
+        job: 'Bartender',
+        salary: '25.000.000'
+      },
+      {
+        job: 'Atlet',
+        salary: '20.000.000'
+      }
+    ]
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  removeJob = (index) => {
+    const {jobs} = this.state
+
+    this.setState({
+      jobs: jobs.filter((r, i) => {
+        return i !== index
+      })
+    })
+  }
+
+  // HANDLER SUBMIT FORM
+  handleSubmit = (jobs) => {
+    const jobState = this.state.jobs
+    this.setState({jobs: [...jobState, jobs] })
+  }
+
+  render () {
+    const {jobs} = this.state
+
+    return (
+      <div className="container">
+        <Table jobData={jobs} removeJob={this.removeJob} />
+        <Form handleSubmit={this.handleSubmit} />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
